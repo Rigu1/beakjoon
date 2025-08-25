d6aa666a9f4f2fd2cd0db277add99d3b4c7ddd5b
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-08-25 10:37:57 +0900

    [Bronze III] Title: 네 번째 점, Time: 136 ms, Memory: 16292 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -1,15 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         ArrayList<int[]> infos = new ArrayList<>();
 
         for (int i = 0; i < 3; i++) {
-            String input = sc.nextLine();
+            String input = br.readLine();
             int[] parts = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
 
             infos.add(parts);
