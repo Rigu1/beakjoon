5aee71aa2b835761f4c4f579f83a67cc4ee38955
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-08-25 11:42:19 +0900

    [Bronze III] Title: 네 번째 점, Time: 120 ms, Memory: 16160 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -6,14 +6,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int x = 0;
         int y = 0;
 
         for (int i = 0; i < 3; i++) {
             String input = br.readLine();
             int[] parts = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).toArray();
-
+            
             x = parts[0] ^ x;
             y = parts[1] ^ y;
         }
