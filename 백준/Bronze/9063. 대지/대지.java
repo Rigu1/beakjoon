526787036188c1d42608a6780c606129e7b234f0
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-08-25 13:24:17 +0900

    [Bronze III] Title: 대지, Time: 252 ms, Memory: 27148 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -2,35 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         int count = Integer.parseInt(br.readLine());
         int xMax = Integer.MIN_VALUE;
         int xMin = Integer.MAX_VALUE;
         int yMax = Integer.MIN_VALUE;
-        int yMin = Integer.MAX_VALUE;
+            int yMin = Integer.MAX_VALUE;
 
-        while (count-- > 0){
-            int[] info = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
+            while (count-- > 0){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
-            if (info[0] > xMax){
-                xMax = info[0];
+                xMax = Math.max(xMax, x);
+                xMin = Math.min(xMin, x);
+                yMax = Math.max(yMax, y);
+                yMin = Math.min(yMin, y);
             }
-            if (info[0] < xMin){
-                xMin = info[0];
-            }
-            if (info[1] > yMax){
-                yMax = info[1];
-            }
-            if (info[1] < yMin){
-                yMin = info[1];
-            }
+
+            System.out.println((xMax - xMin) * (yMax - yMin));
         }
-        System.out.println((xMax - xMin) * (yMax - yMin));
     }
-}
