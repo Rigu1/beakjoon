630e9ed2a65cd70975ac870f2eb9a769778755b3
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-08-25 13:20:52 +0900

    [Bronze III] Title: 대지, Time: 380 ms, Memory: 44752 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,29 +1,36 @@
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int count = Integer.parseInt(br.readLine());
+        int xMax = Integer.MIN_VALUE;
+        int xMin = Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        int yMin = Integer.MAX_VALUE;
 
-            int count = Integer.parseInt(br.readLine());
-            int xMax = Integer.MIN_VALUE;
-            int xMin = Integer.MAX_VALUE;
-            int yMax = Integer.MIN_VALUE;
-            int yMin = Integer.MAX_VALUE;
+        while (count-- > 0){
+            int[] info = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
-            while (count-- > 0){
-                int[] info = Arrays.stream(br.readLine().split(" "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-
-                xMax = Math.max(xMax, info[0]);
-                xMin = Math.min(xMin, info[0]);
-                yMax = Math.max(yMax, info[1]);
-                yMin = Math.min(yMin, info[1]);
+            if (info[0] > xMax){
+                xMax = info[0];
             }
-
-            System.out.println((xMax - xMin) * (yMax - yMin));
+            if (info[0] < xMin){
+                xMin = info[0];
+            }
+            if (info[1] > yMax){
+                yMax = info[1];
+            }
+            if (info[1] < yMin){
+                yMin = info[1];
+            }
         }
+        System.out.println((xMax - xMin) * (yMax - yMin));
     }
+}
