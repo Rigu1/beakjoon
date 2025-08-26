1e4c276038c980433ec2650928eb4631fe1e8715
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-08-26 22:14:19 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine());
+        bw.newLine();
+        bw.write('1');
+        bw.flush();
+    }
+}
