66bf86db6e033d4e7ac45be4b302bc825453accb
rigu1 <49017254+Rigu1@users.noreply.github.com>
2026-01-13 11:21:36 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void setup();
+
+int main() {
+    setup();
+
+    int A, B;
+    cin >> A >> B;
+
+    cout << A + B;
+
+    return 0;
+}
+
+void setup() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
