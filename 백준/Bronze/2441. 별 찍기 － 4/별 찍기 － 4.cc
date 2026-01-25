6b2a8a16a066dc13f213bc04301d08f66a155e70
rigu1 <49017254+Rigu1@users.noreply.github.com>
2026-01-25 15:16:44 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+void setup();
+
+int main() {
+    setup();
+
+    int count;
+    cin >> count;
+
+    string stars(count , '*');
+
+    for (int i = 0; i < count; ++i) {
+        cout << stars << '\n';
+        stars[i] = ' ';
+    }
+
+    return 0;
+}
+
+void setup() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
