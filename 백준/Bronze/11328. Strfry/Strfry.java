17b0c99de8c7e6455175f25e22d10d78265b06ff
rigu1 <49017254+Rigu1@users.noreply.github.com>
2026-03-10 16:41:18 +0900

    [Bronze II] Title: Strfry, Time: 236 ms, Memory: 20392 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -3,28 +3,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int[] counterAlphabet(String text) {
-        int[] counter = new int[26];
-        for (int i = 0; i < text.length(); i++) {
-            int index = text.charAt(i) - 'a';
-            counter[index] += 1;
-        }
-        return counter;
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(bufferedReader.readLine());
+        int[] firstCounter = new int[26];
+        int[] secondCounter = new int[26];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int[] firstCounter = counterAlphabet(stringTokenizer.nextToken());
-            int[] secondCounter = counterAlphabet(stringTokenizer.nextToken());
+
+            fillCounter(stringTokenizer.nextToken(), firstCounter);
+            fillCounter(stringTokenizer.nextToken(), secondCounter);
 
             boolean flag = false;
             for (int j = 0; j < 26; j++) {
@@ -39,8 +34,17 @@ public class Main {
             } else {
                 bufferedWriter.write("Possible\n");
             }
+
+            Arrays.fill(firstCounter, 0);
+            Arrays.fill(secondCounter, 0);
         }
 
         bufferedWriter.flush();
+    }
+
+    private static void fillCounter(String text, int[] counter) {
+        for (int i = 0; i < text.length(); i++) {
+            counter[text.charAt(i) - 'a']++;
+        }
     }
 }
